81442322233e3fa625da1125b9f663cacb3f45b9
apkawa <apkawa@gmail.com>
2017-12-18 14:33:47 +0300

    improve paymaster configuration

--- paymaster/management/commands/paymaster_sync.py
+++ paymaster/management/commands/paymaster_sync.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from six.moves import http_client
+import logging
 import time
 
 import datetime
@@ -11,6 +13,8 @@ from paymaster.models import Refund, Invoice
 
 from paymaster.rest_api.client import PaymasterApiClient
 from paymaster.signals import refund_created, refund_failure, refund_success, invoice_paid
+
+logger = logging.getLogger(__name__)
 
 API_MAP = {
     'SiteInvoiceID': 'number',
@@ -26,6 +30,7 @@ API_MAP = {
     'UserIdentifier': 'payer_id',
 }
 
+
 class Command(BaseCommand):
     help = "Синхронизация оплат и возвратов"
 
@@ -34,14 +39,25 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-                '--loop',
-                action='store_true',
-                dest='loop',
+            '--loop',
+            action='store_true',
+            dest='loop',
+        )
+
+        parser.add_argument(
+            '--debug',
+            action='store_true',
+            dest='debug',
+        )
+        parser.add_argument(
+            '--invoice',
+            action='store',
+            dest='invoice',
         )
 
     @atomic
     def update_payment(self, payment_data):
-        print payment_data
+        logging.debug("update_payment: %s", payment_data)
 
         update_data = {
             'status': payment_data['State'],
@@ -50,12 +66,12 @@ class Command(BaseCommand):
         try:
             invoice = Invoice.objects.get(number=payment_data['SiteInvoiceID'])
         except Invoice.DoesNotExist:
-            print "Not Found"
+            logging.info("Not found invoice: %(SiteInvoiceID)s", **payment_data)
             return None
 
         if invoice.is_finish():
             # Закрытые счета не обновляем
-            print "Already Close"
+            logging.info("Invoice %s already closed", invoice.number)
             return None
 
         if update_data['status'] == Invoice.STATUS_COMPLETE:
@@ -68,13 +84,12 @@ class Command(BaseCommand):
 
         invoice.save()
         if invoice.is_complete():
-            print "SUCCESS"
             invoice_paid.send(sender=self, payer=None, invoice=invoice)
-
+            logging.info("SUCCESS: Invoice %s", invoice.number)
 
     @atomic
     def update_refund(self, refund_data):
-        print refund_data
+        logging.debug("update_refund: %s", refund_data)
         update_data = {
             'status': refund_data['State'],
             'refund_date': refund_data['LastUpdate'],
@@ -90,7 +105,7 @@ class Command(BaseCommand):
             defaults['invoice'] = Invoice.objects.get(payment_id=defaults['payment_id'])
         except Invoice.DoesNotExist:
             # Инвойса нет в нашей системе, отложим до лучших времен
-            print "SKIP", refund_data['RefundID']
+            logging.debug("SKIP: %s", refund_data['RefundID'])
             return None
 
         ext_id = refund_data['ExternalID']
@@ -100,16 +115,16 @@ class Command(BaseCommand):
             created = False
         else:
             refund_obj, created = Refund.objects.get_or_create(
-                    refund_id=refund_data['RefundID'],
-                    defaults=defaults
+                refund_id=refund_data['RefundID'],
+                defaults=defaults
             )
 
         if created:
-            print "CREATED", refund_data['RefundID']
+            logging.debug("CREATED: %s", refund_data['RefundID'])
             refund_created.send(
-                    sender=self,
-                    data=refund_data,
-                    refund=refund_obj
+                sender=self,
+                data=refund_data,
+                refund=refund_obj
             )
         elif refund_obj.is_finish():
             # Возврат уже финиширован, апдейтов не будет
@@ -121,32 +136,39 @@ class Command(BaseCommand):
         refund_obj.save()
 
         if refund_obj.is_success():
-            print "SUCCESS", refund_data['RefundID']
+            logging.debug("SUCCESS: %s", refund_data['RefundID'])
             refund_success.send(
-                    sender=self,
-                    data=refund_data,
-                    refund=refund_obj
+                sender=self,
+                data=refund_data,
+                refund=refund_obj
 
             )
 
         elif refund_obj.is_failure():
-            print "FAILURE", refund_data['RefundID']
+            logging.debug("FAILURE: %s", refund_data['RefundID'])
             refund_failure.send(
-                    sender=self,
-                    data=refund_data,
-                    refund=refund_obj
+                sender=self,
+                data=refund_data,
+                refund=refund_obj
             )
 
-    def sync_open_invoices(self):
-        client = PaymasterApiClient()
-        open_invoices = Invoice.objects.incompleted()
-        for invoice in open_invoices:
+    def sync_invoice(self, invoice):
+        try:
+            logger.debug("SYNC INVOICE %s:", invoice)
+            client = PaymasterApiClient()
             invoice_data = client.get_payment_by_invoice_id(invoice.number)
+            logger.debug("SYNC INVOICE %s: DATA %s", invoice, invoice_data)
             # Тупейший баг
             invoice_data['SiteInvoiceID'] = invoice.number
 
             self.update_payment(invoice_data)
+        except Exception:
+            logger.exception("ERROR SYNC INVOICE: %s", invoice)
 
+    def sync_open_invoices(self):
+        open_invoices = Invoice.objects.incompleted().order_by('-creation_date')
+        for invoice in open_invoices:
+            self.sync_invoice(invoice)
 
     def sync_refund(self):
         client = PaymasterApiClient()
@@ -155,20 +177,36 @@ class Command(BaseCommand):
                 period_from=datetime.datetime.now() - datetime.timedelta(days=3))['Refunds']:
             self.update_refund(row)
 
-    def handle(self, **options):
-
-        # httplib.HTTPConnection.debuglevel = 1
-        #
-        # logging.basicConfig()  # you need to initialize logging, otherwise you will not see anything from requests
-        # logging.getLogger().setLevel(logging.DEBUG)
-        # requests_log = logging.getLogger("requests")
-        # requests_log.setLevel(logging.DEBUG)
-        # requests_log.propagate = True
-
-        self.sync_open_invoices()
-        self.sync_refund()
-
-        while options.get('loop'):
+    def run_loop(self):
+        while True:
             # Run as loop
+            self.sync_open_invoices()
             self.sync_refund()
             time.sleep(10)
+
+    def handle(self, **options):
+        formatter = logging.Formatter('%(asctime)s %(name)-20s %(levelname)-8s %(message)s')
+        # Default handler
+        default_handler = logging.StreamHandler()
+        default_handler.setFormatter(formatter)
+        logger.addHandler(default_handler)
+        logger.setLevel(logging.INFO)
+
+        if options.get('debug'):
+            http_client.HTTPConnection.debuglevel = 1
+            logger.setLevel(logging.DEBUG)
+
+            requests_logger = logging.getLogger("requests")
+            requests_logger.setLevel(logging.DEBUG)
+            requests_logger.propagate = True
+
+        invoice_no = options.get('invoice')
+        if invoice_no:
+            self.sync_invoice(Invoice.objects.get(number=invoice_no))
+            return
+        if options.get('loop'):
+            self.run_loop()
+            return
+        # default
+        self.sync_open_invoices()
+        self.sync_refund()
